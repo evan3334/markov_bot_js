1e74f489b0b3a1a33aa6a519ccfc1090e37a2ff6
Evan <evan.straw99@gmail.com>
2018-08-22 15:05:03 -0700

    Modified format of welcome message slightly

--- main.js
+++ main.js
@@ -21,10 +21,10 @@ let tg = new telegram(cli);
 tg.addCommandListener("start", function (message, args,bot ){
   cli.log("/start command executed by "+cli.getUserFormat(message.from)+" in "+cli.getChatFormat(message.chat));
   let welcomeMsg = "Hi, I'm the Markov Chain bot!\n" +
-    "I can create messages based on Markov Chains, generated from the chat messages in this group.\n" +
+    "I can create messages based on Markov Chains, generated from the chat messages in this group." +
     "Basically, I read the messages in this group and when you run my `/markov` command, I'll use words in your messages that are more likely to follow each other and make a message out of it.\n"+
     "(More on how that works [here](http://setosa.io/ev/markov-chains/).)\n\n" +
-    "My developer is @evan3334, and my code is open source and can be viewed [here](https://github.com/evan3334/markov_bot_js).\n" +
+    "My developer is @evan3334, and my code is open source and can be viewed [here](https://github.com/evan3334/markov_bot_js)." +
     "You may want to visit @evanbotnews to see what's going on with my development or operating status.";
   bot.sendMessage(message.chat.id, welcomeMsg, {parse_mode: "Markdown"});
 });
